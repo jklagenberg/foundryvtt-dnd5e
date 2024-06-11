66bb9d02c0127e26b3a88772d3dc3c624e80cef3
Hoppyhob <Hoppyhob@gmail.com>
2024-06-11 13:53:24 -0500

    [#3530] Added Roll5e to expand base Roll
    Created Roll5e in dice/roll.mjs that extend Roll
    Added Roll5e to dice/_modeule.mjs
    Imported Roll5e in d20-roll.mjs and damage-roll.mjs and modified both to extend Roll5e
    Added Roll5e to Config roll types
    Modified enricher.mjs to use Roll5e in average instead of Roll
    Moved caclDieMaximum from damage-roll to roll.mjs and renamed calcTermMaximum that returns the maximum for a die roll.
    Overwrote roll evaluate and evaluate sync to call replaceDieTermsMaximum to loop through terms and replace die terms with max or min if maximize or minimize is set, then call evaluate as normal.
    Added check in roll to maintain v11 compatibility with evaluate.
    Modified damage-roll.mjs to call evaluateSync on Roll5e to get maximum value.
    closes [#3530]

--- module/dice/damage-roll.mjs
+++ module/dice/damage-roll.mjs
@@ -1,3 +1,5 @@
+import { Roll5e } from "../dice/_module.mjs";
+
 /**
  * A type of Roll specific to a damage (or healing) roll in the 5e system.
  * @param {string} formula                       The string formula to parse
@@ -9,7 +11,7 @@
  * @param {boolean} [options.powerfulCritical=false]  Apply the "powerful criticals" house rule to critical hits
  * @param {string} [options.criticalBonusDamage]      An extra damage term that is applied only on a critical hit
  */
-export default class DamageRoll extends Roll {
+export default class DamageRoll extends Roll5e {
   constructor(formula, data, options) {
     super(formula, data, options);
     if ( !this.options.preprocessed ) this.preprocessFormula();
@@ -130,7 +132,8 @@ export default class DamageRoll extends Roll {
 
           // Powerful critical - maximize damage and reduce the multiplier by 1
           if ( this.options.powerfulCritical ) {
-            let bonus = DamageRoll.calcDieMaximum(term);
+            const MaxRoll = new Roll5e(term.formula, this.data, term.options);
+            let bonus = MaxRoll.evaluateSync({ maximize: true })?.total ?? 0;
             if ( bonus > 0 ) {
               const flavor = term.flavor?.toLowerCase().trim() ?? game.i18n.localize("DND5E.PowerfulCritical");
               flatBonus.set(flavor, (flatBonus.get(flavor) ?? 0) + bonus);
@@ -176,49 +179,6 @@ export default class DamageRoll extends Roll {
 
     // Mark configuration as complete
     this.options.configured = true;
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Gets die information from passed die and calculates the maximum value that could be rolled.
-   *
-   * @param {DiceTerm} die                   DiceTerm to get the maximum value.
-   * @returns {number}                       Maximum value that could be rolled as an integer.
-   */
-
-  static calcDieMaximum(die) {
-    let face = die.faces;
-    let number = die.number;
-    const currentModifiers = foundry.utils.deepClone(die.modifiers);
-    const validModifiers = {
-      k: "keep",
-      kh: "keep",
-      kl: "keep",
-      d: "drop",
-      dh: "drop",
-      dl: "drop",
-      max: "maximum"
-    };
-
-    for ( let modifier of currentModifiers ) {
-      const rgx = /([Mm][Aa][Xx]|[KkDd][HhLl]?)([\d]+)?/i;
-      const match = modifier.match(rgx);
-      if ( !match ) continue;
-      if (match && match[0].length < match.input.length) currentModifiers.push(match.input.slice(match[0].length));
-      let [command, value] = match.slice(1);
-      command = command.toLowerCase();
-      const amount = parseInt(value) || (command === "max" ? -1 : 1);
-
-      if ( command === "max" & amount >= 0 ) {
-        face = amount > face ? face : amount;
-      }
-      else if ( command in validModifiers & amount > 0 ) {
-        number = amount < number ? amount : number;
-      }
-    }
-
-    return face * number;
   }
 
   /* -------------------------------------------- */
